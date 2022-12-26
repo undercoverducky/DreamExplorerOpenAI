ab222a68f291c99b94aeb0eba920be5461837f04
Michael Huang <michaelhuang@Michaels-MBP.attlocal.net>
2022-12-25 23:42:15 -0600

    set openai image to sprite

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -15,18 +15,36 @@ public class PlayerScript : MonoBehaviour
     private string API_KEY = "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";
     private string API_URL = "https://api.openai.com/v1/images/generations";
 
+    public float speed = 10.0f; // Speed of movement
+
     void Start()
+    {
+        
+        StartCoroutine(GetImage());
+    }
+
+    void Update()
+    {
+        // Get input from the 'w', 'a', 's', and 'd' keys
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Use the input to move the game object
+        transform.Translate(new Vector2(horizontalInput, verticalInput) * speed * Time.deltaTime);
+    }
+
+    IEnumerator GetImage()
     {
         // Replace these with your own API key and model name
         string apiKey = API_KEY;
         string model = "image-alpha-001";
 
         // Set the prompt for the image generation
-        string prompt = "A beautiful landscape with mountains in the background and a lake in the foreground.";
+        string prompt = "Pixel RPG Top-Down Perspectiv Character";
 
         // Set the image size
-        int width = 512;
-        int height = 512;
+        int width = 256;
+        int height = 256;
 
         // Set the response format to "url" so that the API returns a URL to the generated image
         string responseFormat = "url";
@@ -47,45 +65,35 @@ public class PlayerScript : MonoBehaviour
         {
             // Get the URL of the generated image from the response
             string imageUrl = response.Content.ReadAsStringAsync().Result;
+            string[] splitresp = imageUrl.Split(':');
+           
+            string url = "https:" + splitresp[4].Split('\"')[0];
+            Debug.Log($"Image generated at URL: {url}");
+            UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+            yield return www.SendWebRequest();
 
-            Debug.Log($"Image generated at URL: {imageUrl}");
+            // Check for errors
+            if (www.isHttpError || www.isNetworkError)
+            {
+                Debug.LogError(www.error);
+                yield break;
+            }
+
+            // Load the texture data into a new Texture2D
+            Texture2D texture = DownloadHandlerTexture.GetContent(www);
+
+            // Create a new sprite from the texture
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+
+            // Assign the sprite to the SpriteRenderer component
+            GetComponent<SpriteRenderer>().sprite = sprite;
+
         }
         else
         {
             Debug.Log($"Error generating image: {response.ReasonPhrase}");
         }
-        //StartCoroutine(GetImage());
-    }
 
-    IEnumerator GetImage()
-    {
-        WWWForm form = new WWWForm();
-        form.AddField("prompt", "Player");
-        form.AddField("n", 1);
-        form.AddField("size", "128x128");
-        // Create a UnityWebRequest and set the request method to POST
-        UnityWebRequest www = UnityWebRequest.Post(API_URL, form);
-
-        // Set the authorization header with the API key
-        www.SetRequestHeader("Authorization", "Bearer " + API_KEY);
-
-        // Set the content type header
-        www.SetRequestHeader("Content-Type", "application/json");
-        Debug.Log("Request: " + www.downloadHandler.text);
-        Debug.Log("Response: " + www.uploadHandler.contentType);
-        // Send the request
-        yield return www.SendWebRequest();
-
-        // Check for errors
-        if (www.isNetworkError || www.isHttpError)
-        {
-            Debug.LogError(www.error);
-        }
-        else
-        {
-            // do stuff
-        }
-        
     }
 }
 
